d1b798569944ecc17e5ee0d2fd7d2f7a5477868e
spaff <spaff1337@gmail.com>
2026-04-14 20:11:12 +0200

    Revert "test"
    This reverts commit f6c9f7dd8ea0e9228d3fc90893b50cec801b02ad.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 
-// compile cmd
 // dotnet publish -c Release -r win-x86 --self-contained true
 
 class TerrariaPatcher {
